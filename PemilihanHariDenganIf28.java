087e8360a2d729f40960b4f633772f042c2c891e
SheilandraZarawiba29 <Sheilandrazarawiba561@gmail.com>
2024-09-26 10:06:27 +0700

    perubahan nama file

--- PemilihanHariDenganIf28.java
+++ PemilihanHariDenganIf28.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PemilihanHariDenganIf {
+public class PemilihanHariDenganIf28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
